a137095d6ba87225be0e7f19e6de9df3405ebf8f
Shi Heng Ng <ngshiheng@gmail.com>
2020-01-04 20:26:46 +0800

    Add defaultPlaygroundQuery and options to GraphQLServer

--- server/src/index.js
+++ server/src/index.js
@@ -8,6 +8,24 @@ const resolvers = {
     Mutation,
 };
 
+const defaultLoginQuery = `# Enter your <email> and <password> to login to receive your access token
+mutation {
+  login(email: "<email>", password: "<password>") {
+    token
+    user {
+      id
+      role
+    }
+  }
+}
+
+# Replace <paste access token here> with your access token
+# Paste the authorization header below in the 'HTTP HEADERS' tab at the bottom of the page for all your requests
+#   {
+#        Authorization: 'Bearer <paste access token here>',
+#   }
+`;
+
 const server = new GraphQLServer({
     typeDefs: './src/schemas/Users/schema.graphql', // TODO: Bind all typeDefs & resolvers from different objects
     resolvers,
@@ -20,6 +38,15 @@ const server = new GraphQLServer({
     },
 });
 
-server.start(() =>
-    console.log(`🚀  Server is running on http://localhost:4000`),
+const options = {
+    port: 4000,
+    endpoint: '/graphql',
+    playground: '/graphql',
+    defaultPlaygroundQuery: defaultLoginQuery,
+};
+
+server.start(options, () =>
+    console.log(
+        `🚀  Server is running on http://localhost:${options.port}/graphql`,
+    ),
 );
