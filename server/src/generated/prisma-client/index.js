89126d22f0212e4e531741b2342b6d3ba5c0d10d
Jerry Ng <ngshiheng@gmail.com>
2020-01-05 22:53:57 +0800

    Git fixes

---
+++ server/src/generated/prisma-client/index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "Role",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://us1.prisma.sh/jerry-ng-9be71a/oneone/dev`
+});
+exports.prisma = new exports.Prisma();
