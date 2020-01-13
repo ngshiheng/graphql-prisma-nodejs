cee45c3b441dfaa1e25cb20072b428abe18c2239
Jerry Ng <ngshiheng@gmail.com>
2020-01-13 22:03:57 +0800

    Update instruction for docker

--- server/run.sh
+++ server/run.sh
@@ -5,5 +5,6 @@ DOCKER_CONTAINER_NAME="yourcontainername"
 
 echo "🐳  Running container from image: ${DOCKER_IMAGE_NAME}"
 docker run -d --name ${DOCKER_CONTAINER_NAME} -p 4000:4000 ${DOCKER_IMAGE_NAME}
-docker ps
 echo "🚀  Server is running on http://localhost:4000/graphql"
+echo "🛑  To stop the container run 'docker stop ${DOCKER_CONTAINER_NAME}'"
+echo "🏃‍♀️  To start the container run 'docker start ${DOCKER_CONTAINER_NAME}'"
